8e4b9fb5f8a3df531ead9d42b42da3aacd9046a9
Pasternak-clase <114140701+Pasternak-clase@users.noreply.github.com>
2022-10-17 12:21:58 +0200

    Add files via upload

---
+++ Entornos/2-holamundo-main/holamundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace aplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adios mundo");
+
+        }
+    }
+}
---
+++ Entornos/5-depuracionVSC-main/Dep.C/depuracionC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace entornos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 234;
+            int num2 = 40;
+
+            int suma = num + num2;
+            int multiplicacion = num * num2;
+            int division = num / num2;
+            
+            Console.WriteLine($"{num}, {suma}, {multiplicacion}, {division}");
+        }
+    }
+}
