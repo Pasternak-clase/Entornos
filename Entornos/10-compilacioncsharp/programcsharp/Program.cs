675e18ab6fa8dc603329fad97ee6c9d439bc353f
Pasternak-clase <114140701+Pasternak-clase@users.noreply.github.com>
2022-10-27 18:56:27 +0200

    Add files via upload

---
+++ Entornos/10-compilacioncsharp/programcsharp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace nombrec{
+class program{
+    public static void Main(String[] args){
+        string nombre = Console.ReadLine();
+        System.Console.WriteLine("Hola " + nombre);
+    }
+}
+}
