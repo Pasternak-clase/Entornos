f1ace02564cdc4a7dbd953bd26a25513a55ebf53
Pasternak-clase <114140701+Pasternak-clase@users.noreply.github.com>
2022-12-14 09:17:43 +0100

    Add files via upload

---
+++ Entornos/15-netbeansphp/phpnetbeans/formphp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $nombre = $_POST["nombre"];
+            $apellido = $_POST["apellido"];
+            echo "hola $nombre $apellido";
+        ?>
+    </body>
+</html>
